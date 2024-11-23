8c85ba3bb1856763710805b526f946f1e63eccbe
AdanRuvalcab4 <christian.ruvalcaba0918@alumnos.udg.mx>
2024-11-23 16:39:37 -0600

    imagenes

--- resources/views/libros/index-libros.blade.php
+++ resources/views/libros/index-libros.blade.php
@@ -20,6 +20,7 @@
                 <th>Autor</th>
                 <th>Precio</th>
                 <th>Stock</th>
+                <th>Libro</th>
                 <th>Acciones</th>
             </tr>
         </thead>
@@ -34,6 +35,13 @@
                 <td>{{ $libro->autor }}</td>
                 <td>{{ $libro->precio }}</td>
                 <td>{{ $libro->stock }}</td>
+                <td>
+                    @if($libro->imagen)
+                        <img src="/storage/{{ $libro->imagen }}"  width="100">
+                    @else
+                        <p>No hay imagen</p>
+                    @endif
+                </td>
                 <td>
                     <a href="{{ route('libro.edit', $libro) }}">Editar</a>
                 </td>
